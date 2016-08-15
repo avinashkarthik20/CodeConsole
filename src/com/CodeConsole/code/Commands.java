43d64725e577e827886bf999c4b4625bbefa6ce4
Avinash Karthik Siva Subramanian <avinashkarthik.sivasubramanian@yahoo.de>
2016-08-15 06:30:39 +0200

    Commit 2: Code for creating canvas and displaying it

---
+++ src/com/CodeConsole/code/Commands.java
@@ -0,0 +1,11 @@
+package com.CodeConsole.code;
+
+public enum Commands {
+	
+	C,
+	L,
+	R,
+	B,
+	Q;
+	
+}
---
+++ src/com/CodeConsole/code/DrawingApplication.java
@@ -0,0 +1,35 @@
+package com.CodeConsole.code;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DrawingApplication {
+
+	static Canvas canvas;
+	
+	public static void main(String[] args) {		
+		Scanner scan = new Scanner(System.in);
+		String command = new String();
+		while(!command.equals("Q")) {
+			System.out.print("Enter command:");
+			command = scan.nextLine();
+			draw(command);
+		}
+		System.out.println("Exiting Program");
+		scan.close();
+	}
+	
+	private static void draw(String command){
+		Map<Integer, Commands> map = new HashMap<Integer, Commands>();
+		map.put(0, Commands.C);
+		if(String.valueOf(command.charAt(0)).equals(map.get(0).toString())){
+					String[] cmd = command.split(" ");
+					canvas.createCanvas(Integer.parseInt(cmd[1]),Integer.parseInt(cmd[2]));
+					canvas.display();
+		}
+    }
+} 
+		
+
+
